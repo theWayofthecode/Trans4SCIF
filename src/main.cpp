1a93fd7b6ffdab7dbbb72f4e8bec7270e618c83b
Aram Santogidis <aram.santogidis@cern.ch>
2015-11-09 11:48:57 +0100

    Added the License file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+/*
+	© Copyright 2015 CERN
+	
+	This software is distributed under the terms of the 
+	GNU General Public Licence version 3 (GPL Version 3), 
+	copied verbatim in the file “LICENSE”.
+	In applying this licence, CERN does not waive 
+	the privileges and immunities granted to it by virtue of its status 
+	as an Intergovernmental Organization or submit itself to any jurisdiction.
+	
+	Author: Aram Santogidis <aram.santogidis@cern.ch>
+*/
+
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
